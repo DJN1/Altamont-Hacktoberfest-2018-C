65637d3fd7fc5743fdde313a0eecd5e76bb919de
Ryan James <ryanjames1729@gmail.com>
2018-10-03 08:46:33 -0500

    added a new line - my name

--- Altamont-Octoberfest2018-C/Altamont-Hacktoberfest2018-C.c
+++ Altamont-Octoberfest2018-C/Altamont-Hacktoberfest2018-C.c
@@ -4,7 +4,8 @@ int main()
 {
 	printf("Hello, my name is David Niederweis\n");
 	// Add your code below.
-	
+	char[20] teacher = "Mr. James";
+	printf("Hello, this is %s adding some code", teacher);
 	
 	
 	
